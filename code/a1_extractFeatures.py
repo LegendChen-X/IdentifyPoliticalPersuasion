69d50413bda6448373d0585d82d750896d37af7f
Xiang <chenxiang00825@gmail.com>
2021-02-09 19:17:21 -0500

    done without check

--- code/a1_extractFeatures.py
+++ code/a1_extractFeatures.py
@@ -11,6 +11,8 @@
 import numpy as np
 import argparse
 import json
+import string
+import re
 
 # Provided wordlists.
 FIRST_PERSON_PRONOUNS = {
@@ -27,17 +29,17 @@ SLANG = {
     'afn', 'bbs', 'cya', 'ez', 'f2f', 'gtr', 'ic', 'jk', 'k', 'ly', 'ya',
     'nm', 'np', 'plz', 'ru', 'so', 'tc', 'tmi', 'ym', 'ur', 'u', 'sol', 'fml'}
     
-    
-def readFile(path):
-    with open(path, "r") as ouf: lines = ouf.read().splitlines()
-    return lines
-    
-path = "./../feats/"
-    
-altFeats = np.load(path + "Alt_feats.dat.npy")
-centerFeats = np.load(path + "Center_feats.dat.npy")
-leftFeats = np.load(path + "Left_feats.dat.npy")
-rightFeats = np.load(path + "Right_feats.dat.npy")
+BGLpath = "./../Wordlists/BristolNorms+GilhoolyLogie.csv"
+WARpath = "./../Wordlists/Ratings_Warriner_et_al.csv"
+altPathData = "./../feats/Alt_IDs.txt"
+leftPathData = "./../feats/Left_IDs.txt"
+rightPathData = "./../feats/Right_IDs.txt"
+centerPathData = "./../feats/Center_IDs.txt"
+
+altPathArray = "./../feats/Alt_feats.dat.npy"
+leftPathArray = "./../feats/Left_feats.dat.npy"
+rightPathArray = "./../feats/Right_feats.dat.npy"
+centerPathArray = "./../feats/Center_feats.dat.npy"
 
 
 def extract1(comment):
@@ -48,14 +50,120 @@ def extract1(comment):
 
     Returns:
         feats : numpy Array, a 173-length vector of floating point features (only the first 29 are expected to be filled, here)
-    '''    
+    '''
+    fp = open(BGLpath,"r")
+    BGLdata = fp.read().split("\n")[1:-3]
+    fp.close()
+    BGL = {}
+    for line in BGLdata:
+        element = line.split(",")
+        BGL[element[1]] = [element[3], element[4], element[5]]
+        
+    fp = open(WARpath, "r")
+
+    WARdata = fp.read().split('\n')[1:-1]
+    WAR = {}
+    for line in WARdata:
+        element = line.split(",")
+        WAR[element[1]] = [element[2], element[5], element[8]]
+        
+        
     # TODO: Extract features that rely on capitalization.
     features = np.zeros(173)
-    body = re.compile("(\S+)/(?=\S+)").findall(comment)
-    lemma = re.compile("(?<=\S)/(\S+)").findall(comment)
+    data = re.compile("(\S+)/(?=\S+)").findall(comment)
+    tags = re.compile("(?<=\S)/(\S+)").findall(comment)
+    # TODO: Extract features that rely on capitalization.
+    for text in data:
+        if text.isupper() and len(text) >= 3: features[0] += 1
+        
     # TODO: Lowercase the text in comment. Be careful not to lowercase the tags. (e.g. "Dog/NN" -> "dog/NN").
+    main = [text.lower() for text in data]
     # TODO: Extract features that do not rely on capitalization.
+        
+    features[4] = tags.count('CC')
+    features[5] = tags.count('VBD')
     
+    features[6] += len(re.compile(r'\b(' + r'|'.join(['\'ll', 'will', 'shall', 'gonna']) + r')\b').findall(comment))
+    
+    features[6] += len(re.compile(r"go/VBG to/TO [\w]+/VB").findall(comment))
+    
+    features[7] = re.compile("(?=/)[\S]+").sub('', comment).count(',')
+    
+    features[8] = len(re.findall(' \W{2,}/', comment))
+    
+    features[9] = tags.count('NN') + tags.count('NNS')
+    
+    features[10] = tags.count('NNP') + tags.count('NNPS')
+    
+    features[11] = tags.count('RB') + tags.count('RBR') + tags.count('RBS')
+    
+    features[12] = tags.count('WDT') + tags.count('WP$') + tags.count('WP') + tags.count('WRB')
+    
+    for text in main:
+        if text in FIRST_PERSON_PRONOUNS: features[1] += 1
+        if text in SECOND_PERSON_PRONOUNS: features[2] += 1
+        if text in THIRD_PERSON_PRONOUNS: features[3] += 1
+        if text in SLANG: features[13] += 1
+        
+    breaks = comment.count('\n')
+    
+    if not breaks: breaks += 1
+    
+    features[14] = len(main) / breaks
+    
+    count = 0
+    
+    length = 0
+    
+    for text in main:
+        if not set(text).issubset(set(string.punctuation)):
+            count += 1
+            length += len(text)
+            
+    if not count: count += 1
+    
+    if main: features[15] = length / count
+    else: features[15] = 0
+    
+    features[16] = breaks
+    
+    AoA = []
+    IMG = []
+    FAM = []
+    Vs = []
+    As = []
+    Ds = []
+    
+    for text in main:
+        if text in BGL:
+            AoA.append(BGL[text][0])
+            IMG.append(BGL[text][1])
+            FAM.append(BGL[text][2])
+        if text in WAR:
+            Vs.append(WAR[text][0])
+            As.append(WAR[text][1])
+            Ds.append(WAR[text][2])
+            
+    AoA = np.asarray(AoA, np.float32)
+    IMG = np.asarray(IMG, np.float32)
+    FAM = np.asarray(FAM, np.float32)
+    Vs = np.asarray(Vs, np.float32)
+    As = np.asarray(As, np.float32)
+    Ds = np.asarray(Ds, np.float32)
+    features[17] = np.mean(AoA)
+    features[18] = np.mean(IMG)
+    features[19] = np.mean(FAM)
+    features[20] = np.std(AoA)
+    features[21] = np.std(IMG)
+    features[22] = np.std(FAM)
+    features[23] = np.mean(Vs)
+    features[24] = np.mean(As)
+    features[25] = np.mean(Ds)
+    features[26] = np.std(Vs)
+    features[27] = np.std(As)
+    features[28] = np.std(Ds)
+    
+    return features
     
     
 def extract2(feat, comment_class, comment_id):
@@ -71,16 +179,41 @@ def extract2(feat, comment_class, comment_id):
         function adds feature 30-173). This should be a modified version of 
         the parameter feats.
     '''
-    if comment_class == "Alt": feats[29:173] = alt_feats[alt_IDs.index(comment_id)]
+    
+    altArray = np.load(altPathArray)
+    leftArray = np.load(leftPathArray)
+    rightArray = np.load(rightPathArray)
+    centerArray = np.load(centerPathArray)
+    altIDs = {}
+    leftIDS = {}
+    rightIDs = {}
+    centerIDs= {}
+    altFp = open(altPathData, "r")
+    leftFp = open(leftPathData, "r")
+    rightFp = open(rightPathData, "r")
+    centerFp = open(centerPathData, "r")
+    altData = altFp.read().split()
+    altFp.close()
+    leftData = leftFp.read().split()
+    leftFp.close()
+    rightData = rightFp.read().split()
+    rightFp.close()
+    centerData = centerFp.read().split()
+    centerFp.close()
+    for i in range(len(altData)): altIDs[altData[i]] = i
+    for i in range(len(leftData)): leftIDS[leftData[i]] = i
+    for i in range(len(rightData)): rightIDs[rightData[i]] = i
+    for i in range(len(centerData)): centerIDs[centerData[i]] = i
+    
+    if comment_class == "Alt": feat = np.append(feat[:29], altArray[altIDs[comment_id]])
         
-    elif comment_class == "Center":
+    elif comment_class == "Left": feat = np.append(feat[:29], leftArray[leftIDs[comment_id]])
         
-    elif comment_class == "Left":
+    elif comment_class == "Right": feat = np.append(feat[:29], rightArray[rightIDs[comment_id]])
+    
+    elif comment_class == "Center": feat = np.append(feat[:29], centerArray[centerIDs[comment_id]])
         
-    elif comment_class == "Right":
-        
-    return feats
-    print('TODO')
+    return feat
 
 
 def main(args):
@@ -96,7 +229,15 @@ def main(args):
     # into feats. (Note that these rely on each data point's class,
     # which is why we can't add them in extract1).
     
-    for i in range()
+    for i in range(len(data)):
+        res = extract1(data[i]["body"])
+        if data[i]["cat"] == "Left": feats[i][-1] = 0
+        elif data[i]["cat"] == "Center": feats[i][-1] = 1
+        elif data[i]["cat"] == "Right": feats[i][-1] = 2
+        elif data[i]["cat"] == "Alt": feats[i][-1] = 3
+        res = extract2(res, data[i]["cat"], data[i]['id'])
+        feats[i,:-1] = res
+    
 
     np.savez_compressed(args.output, feats)
 
